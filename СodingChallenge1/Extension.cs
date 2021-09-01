89387b17b19a63ae0c9767a5e1534df8ef7e6ebb
Danylo Lebets <dlebe@softserveinc.com>
2021-09-01 17:36:47 +0300

    Add extension method to insert to linked list

---
+++ СodingChallenge1/Extension.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace СodingChallenge1
+{
+    public static class Extension
+    {
+        public static LinkedList<int> Insert(this LinkedList<int> list, int node, int position)
+        {
+            int first = list.First.Value;
+            list.RemoveFirst();
+
+            if (position == 1)
+            {
+                list.AddFirst(first);
+                list.AddFirst(node);
+            }
+            else
+            {
+                list.Insert(node, position - 1);
+                list.AddFirst(first);
+            }
+
+            return list;
+        }
+    }
+}
--- СodingChallenge1/Program.cs
+++ СodingChallenge1/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace СodingChallenge1
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            const int node = 666;
+            const int position = 9;
+            
+            LinkedList<int> list = CreateFilledLinkedList(10);
+            ShowLinkedList(list);
+
+            if (position > 1 && position < list.Count)
+            {
+                list.Insert(node, position);
+            }
+            ShowLinkedList(list);
+
+
+            Console.ReadKey();
+        }
+
+        static LinkedList<int> CreateFilledLinkedList(int maxNumber)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            for (int i = 0; i < maxNumber; i++)
+            {
+                list.AddLast(i);
+            }
+
+            return list;
+        }
+
+        static void ShowLinkedList(LinkedList<int> list)
+        {
+            int arrowSize = 3;
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (int number in list)
+            {
+                output.AppendFormat($"{number} -> ");
+            }
+
+            output.Remove(output.Length - arrowSize, arrowSize);
+
+            Console.WriteLine(output.ToString());
         }
     }
 }
